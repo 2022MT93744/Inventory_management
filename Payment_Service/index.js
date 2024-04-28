2e409d8c336d93839aff47666849dc756bbb392f
2022MT93744 <127505741+2022MT93744@users.noreply.github.com>
2024-04-28 20:54:32 +0530

    Update index.js

--- Payment_Service/index.js
+++ Payment_Service/index.js
@@ -30,7 +30,7 @@ app.get('/paydetails', (req, res) => {
       client.connect();
   }
 
-  client.query('SELECT * FROM invpayment', (err, result) => {
+  client.query('SELECT * FROM invapayment', (err, result) => {
       if (err) {
           console.error(err);
           res.status(500).json({ error: 'An error occurred' });
@@ -42,6 +42,57 @@ app.get('/paydetails', (req, res) => {
       // Ensure the client is closed after the query
   });
 });
+
+
+//////////////////////////////////////////All user get////////////////////////////////////////////////////////
+app.get('/cretable', (req, res) => {
+    if (client._connected) {
+        // Nothing
+    } else {
+        client.connect();
+    }
+  
+    client.query('CREATE TABLE IF NOT EXISTS public.invapayment("P_ID" character(10) COLLATE pg_catalog."default" NOT NULL,"P_Amount" numeric(8,0),"P_type" character(10) COLLATE pg_catalog."default","P_date" date,CONSTRAINT invapayment_pkey PRIMARY KEY ("P_ID"))', (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'An error occurred' });
+        } else {
+            //datad3 = result.rows;
+            console.log("table created in UI");
+            //res.render('paydetails', { datad3 }); // Render the EJS template with the data
+        }
+        // Ensure the client is closed after the query
+    });
+    res.sendFile(__dirname + "/UI/paydetailsf.html");
+  });
+//////////////////////////////////////////All user get////////////////////////////////////////////////////////
+app.get('/instdata', (req, res) => {
+
+    const P_ID = 'I00121';
+    const P_Amount = '457';
+    const P_type = 'Online';
+    const P_date = '01/01/2022';
+
+    if (client._connected) {
+        // Nothing
+    } else {
+        client.connect();
+    }
+  
+    client.query('INSERT INTO "invapayment" VALUES ($1, $2, $3, $4)',[P_ID,P_Amount,P_type,P_date], (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'An error occurred' });
+        } else {
+            //datad3 = result.rows;
+            console.log("Payment displayed in UI");
+            //res.render('paydetails', { datad3 }); // Render the EJS template with the data
+        }
+        // Ensure the client is closed after the query
+    });
+    res.sendFile(__dirname + "/UI/paydetailsf.html");
+  });
+  
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
